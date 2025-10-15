4ccf240404af623096e5269aa2e95ef9d7375691
surya-tn99 <balasurya.tn99@gmail.com>
2025-10-15 22:43:14 +0530

    user's video will be saved with custom id for avoiding the same filename conflict

--- src/main.py
+++ src/main.py
@@ -9,11 +9,26 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 import uvicorn
 
+# file ID preparation
+from datetime import datetime
+import random
+import csv
+
 app = FastAPI()
 
 user_video_saving_dir = "data/users"
 os.makedirs(user_video_saving_dir, exist_ok=True)
-    
+
+# file to store the video file name
+csv_file_path = "data/videos/videoName.csv"
+os.makedirs(os.path.dirname(csv_file_path), exist_ok=True)
+
+# Ensure CSV file exists with header
+if not os.path.exists(csv_file_path):
+    with open(csv_file_path, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["id", "filename"]) 
+
 
 templates = Jinja2Templates(directory = "website")
 app.mount("/style", StaticFiles(directory="website/style"), name="style")
@@ -25,11 +40,26 @@ async def mainPage(request:Request):
 @app.post("/upload")
 async def storeVideo(videoFile : UploadFile = File(...)):
 
-    file_path = os.path.join(user_video_saving_dir, videoFile.filename)
-    
+    def prepare_file_id():
+        
+        timestamp = datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
+        uuid4 = f"{random.randint(1000, 9999)}"
+        extension = os.path.splitext(videoFile.filename)[1]
+        return  f"{timestamp}-{uuid4}{extension}"
+        
     try:
+        
+        ID = prepare_file_id()
+
+        file_path = os.path.join(user_video_saving_dir, ID )
+    
         with open(file_path, 'wb') as FILE:
             shutil.copyfileobj(videoFile.file , FILE)
+        
+
+        with open(csv_file_path,"a", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow([ID, videoFile.filename])
         
         return {
             "status":"video uploaded successfully",
